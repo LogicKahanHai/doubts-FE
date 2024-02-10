5de1b87880dc9779c047a0be1378aba675583661
Rishi Bhalla <logic@Rishis-MacBook-Pro.local>
2024-02-11 03:09:48 +0530

    Updating Time UI

--- src/components/questions/Question.jsx
+++ src/components/questions/Question.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const Question = ({ user }) => {
     let date = new Date(user.time);
     let showDate = date.toDateString();
-    let showTime = date.toLocaleTimeString().replace(/:\d+ /, ' ');
+    const showTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const amOrPm = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }).slice(-2);
     return (
         <div className="question-card">
             <div className="user-details">
                 {/* <img src={user.avatar} alt={user.name} className="avatar" /> */}
-                <span className="username"><strong>{user.name}</strong> - <i>{showDate} @ {showTime}</i></span>
+
+                <span className="username"><strong>{user.name}</strong> - <i>{showDate} @ {showTime} {amOrPm}</i></span>
             </div>
             <div className="question-text">{user.msg}</div>
         </div>
